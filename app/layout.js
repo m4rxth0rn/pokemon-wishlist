4374a491e69eaa27c7936f44f8523a5475d5baab
Mari <dominikzidlicky@seznam.cz>
2025-05-17 21:45:27 +0200

    4.2
    Oprava kodu

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,15 @@
-import './globals.css';
-import Navbar from './components/Navbar';
+import "./globals.css";
+import Navbar from "./components/Navbar";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="cs">
       <head>
-        <meta name="viewport" content="width=1280, initial-scale=1.0" />
-        <title>Moje Aplikace</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=1024" />
+        <title>Moje stránka</title>
       </head>
       <body>
-        {/* 🧩 JavaScript fallback pro "zoom-out" na malých obrazovkách */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function () {
-                if (window.innerWidth < 768) {
-                  document.body.style.transform = 'scale(0.3)';
-                  document.body.style.transformOrigin = 'top left';
-                  document.body.style.width = '3333px';
-                }
-              })();
-            `
-          }}
-        />
-        
         <Navbar />
         {children}
       </body>
